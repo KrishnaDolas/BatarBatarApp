d4eb9825fe476c4f55381585d79833e0a7181f60
KrishnaDolas <kdolas99@gmail.com>
2024-04-07 00:03:30 +0530

    socket url updated and logs added for messages and user join and disconnect

--- index.js
+++ index.js
@@ -13,17 +13,20 @@ io.on('connection', socket =>{
     socket.on('new-user-joined', name =>{ 
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
+        console.log(`User Joined ${name}`)
     });
 
     // If someone sends a message, broadcast it to other people
     socket.on('send', message =>{
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+        console.log(`Message sent ${message}`)
     });
 
     // If someone leaves the chat, let others know 
     socket.on('disconnect', message =>{
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
+        console.log(`User disconnected ${socket.id}`)
     });
 
 
